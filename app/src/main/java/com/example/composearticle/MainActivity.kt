8828d2c29d943725a74e4f1fc0d02e8e338d0a0b
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-01-27 16:21:01 +0000

    feat: Implemented all the codes and finished building the app

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,29 +30,51 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    ArticleImage(title = stringResource(R.string.article_title))
+                    ArticleImage(
+                        title = stringResource(R.string.article_title),
+                        article1 = stringResource(R.string.article_1),
+                        article12 = stringResource(R.string.article_2)
+                    )
                 }
             }
         }
     }
 }
 
-
-// article1: String, article2: String,
 @Composable
-fun ArticleText(title: String, modifier: Modifier = Modifier) {
-    Text(
-        text = title,
-        fontSize = 24.sp,
-        modifier = Modifier
-            .padding(16.dp)
-    )
-
+fun ArticleText(
+    title: String,
+    article1: String,
+    article12: String,
+    modifier: Modifier = Modifier
+) {
+    Column {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = article1,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = article12,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+    }
 }
 
 @Composable
 fun ArticleImage(
     title: String,
+    article1: String,
+    article12: String,
     modifier: Modifier = Modifier
 ) {
     val image = painterResource(R.drawable.bg_compose_background)
@@ -62,7 +85,9 @@ fun ArticleImage(
             contentDescription = null
         )
         ArticleText(
-            title = title
+            title = title,
+            article1 = article1,
+            article12 = article12
         )
     }
 }
@@ -71,6 +96,9 @@ fun ArticleImage(
 @Composable
 fun DefaultPreview() {
     ComposeArticleTheme {
-        ArticleImage(title = stringResource(R.string.article_title))
+        ArticleImage(
+            title = stringResource(R.string.article_title),
+            article1 = stringResource(R.string.article_1),
+            article12 = stringResource(R.string.article_2))
     }
 }
